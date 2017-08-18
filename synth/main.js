91b70dced0afd0b8acd35e2376848de9e780c34c
Will Munn <willmunn@hotmail.com>
2017-08-18 22:52:56 +0100

    control waveform

--- synth/main.js
+++ synth/main.js
@@ -1,31 +1,62 @@
 var Synth = require('./synth');
 
 function main () {
-    function tieSliderToPitch(id, o) {
-        var slider = document.getElementById(id);
-        o.frequency.value = slider.value;
+    var synthElement = document.getElementById('synth');
+    function registerOscillatorControls(synth, id, oscIndex) {
+        var prefix = 'osc-' + id;
+        var oscControls = document.getElementById(prefix);
+        var waveformSelector = document.getElementsByName(prefix + '-waveform');
+        var slider = document.getElementById(prefix + '-detune');
+        synth.events.detune(oscIndex, slider.value);
         slider.oninput = function (e) {
             console.log(e.target.value);
-            o.frequency.value = e.target.value;
+            var evt = synth.events.detune(oscIndex, e.target.value);
+            synthElement.dispatchEvent(evt);
+        }
+        waveformSelector.forEach(function (i) {
+            i.onchange = function (e) {
+                var evt = synth.events.waveform(oscIndex, e.target.value);
+                synthElement.dispatchEvent(evt);
+                console.log(evt.target.value);
+            }
+        });
+    }
+
+    function connectKeyboard (synth) {
+        document.onkeydown = function (e) {
+            function frequencyFromNoteNumber( note ) {
+                return 440 * Math.pow(2,(note-69)/12);
+            }
+
+            var keyboard = 'awsedftgyhu'.split('');
+            if (keyboard.indexOf(e.key) != -1) {
+                var midiNote = 60 + keyboard.indexOf(e.key);
+                var freq = frequencyFromNoteNumber(midiNote);
+                console.log(e.key, freq);
+                synth.freq(freq);
+            }
         }
     }
 
     function createSynth (stopButton) {
-        var synth = new Synth(ac);
-        tieSliderToPitch('osc1-pitch', synth.osc1);
-        tieSliderToPitch('osc2-pitch', synth.osc2);
+        var synth = new Synth(ac, synthElement);
+        registerOscillatorControls(synth, '1', 0);
+        registerOscillatorControls(synth, '2', 1);
         stopButton.onclick = synth.stop.bind(synth);
+        connectKeyboard(synth);
         return synth;
     }
 
     var ac = new AudioContext();
     var stopButton = document.getElementById('stop');
     var start = document.getElementById('start');
-
+    var synth;
     start.onclick = function () {
-        var synth = createSynth(stopButton);
+        if (synth) {synth.stop();}
+        synth = createSynth(stopButton);
         synth.start();
     }
+
 }
 
 document.onload = window.onload = main;
--- synth/synth.js
+++ synth/synth.js
@@ -1,20 +1,81 @@
-function Synth(ac) {
-    this.osc1 = createOsc(ac);
-    this.osc2 = createOsc(ac);
+function Synth(ac, element) {
+    this._freq = 440;
+    this.osc1 = {
+        osc:createOsc(ac),
+        detune: 0
+    };
+    this.osc2 = {
+        osc: createOsc(ac),
+        detune: 0
+    };
     this.oscs = [this.osc1, this.osc2];
+    if (element) {
+        ['detune', 'waveform'].forEach(function (eventType) {
+            element.addEventListener(eventType, function (e) {
+                this[eventType](e.detail)
+            }.bind(this));
+        }.bind(this));
+    }
 }
 
 Synth.prototype.start = function () {
     this.oscs.forEach(function (o) {
-        o.start();
+        o.osc.start();
     });
 }
 
 
 Synth.prototype.stop = function () {
     this.oscs.forEach(function (o) {
-        o.stop();
+        o.osc.stop();
     });
+}
+
+Synth.prototype._guardOscIndex = function (oscIndex) {
+    if (oscIndex < 0 || oscIndex > this.oscs.length) {
+        throw new Error('Invalid oscillator specified: ' + oscIndex);
+    }
+}
+
+Synth.prototype.waveform = function (opts) {
+    this._guardOscIndex(opts.oscIndex);
+    console.log('changed waveform:' + opts.waveform);
+    this.oscs[opts.oscIndex].osc.type = opts.waveform;
+}
+
+Synth.prototype.detune = function (opts) {
+    this._guardOscIndex(opts.oscIndex);
+    console.log(opts.hertz);
+    this.oscs[opts.oscIndex].detune = opts.hertz;
+    this.freq(this._freq);
+}
+
+Synth.prototype.freq = function (freq) {
+    this._freq = freq;
+    this.oscs.forEach(function (o) {
+        var adjustedFreq = (freq + Number(o.detune));
+        console.log(adjustedFreq);
+        o.osc.frequency.value = adjustedFreq;
+    });
+}
+
+Synth.prototype.events = {
+    detune: function (oscIndex, hertz) {
+        return new CustomEvent('detune', {
+            detail: {
+                oscIndex: oscIndex,
+                hertz: hertz
+            }
+        });
+    },
+    waveform: function (oscIndex, waveform) {
+        return new CustomEvent('waveform', {
+            detail: {
+                oscIndex: oscIndex,
+                waveform: waveform
+            }
+        });
+    },
 }
 
 function createOsc(ac) {
